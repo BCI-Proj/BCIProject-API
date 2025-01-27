fc51b8e93ca7b3fc5bb9c7839a8f2a4b0ffa73a7
Raaphe <120033739+Raaphe@users.noreply.github.com>
2025-01-27 09:45:02 -0500

    init create

---
+++ src/main/java/bciapi/bciapi/BciApiApplication.java
@@ -0,0 +1,13 @@
+package bciapi.bciapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BciApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bciapi/bciapi/BciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bciapi.bciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
